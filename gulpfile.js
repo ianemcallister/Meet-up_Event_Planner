3cbc4f24b2b8da0dc4e374dade8d0116bbc9b99b
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2016-01-29 15:23:14 +0900

    linting, browserSync, sass and autoprefixer all running correctly

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,50 @@
+/*eslint-env node */
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
+var eslint = require('gulp-eslint');
 
 gulp.task('default', ['copy-html', 'copy-images', 'styles', 'lint'], function() {
-	//default tasks go here
 	gulp.watch('sass/**/*.scss', ['styles']);
+	gulp.watch('js/**/*.js', ['lint']);
+	gulp.watch('/index.html', ['copy-html']);
+	gulp.watch('./dist/index.html')
+		.on('change', browserSync.reload);
 
 	browserSync.init({
 		server: './dist'
 	});
 });
+
+gulp.task('dist', [
+	'copy-html',
+	'copy-images',
+	'styles',
+	'lint',
+	'scripts-dist'
+]);
+
+gulp.task('scripts', function() {
+	gulp.src('js/**/*.js')
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('scripts-dist', function() {
+	gulp.src('js/**/*.js')
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('copy-html', function() {
+	gulp.src('./index.html')
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy-images', function() {
+	gulp.src('img/*')
+		.pipe(gulp.dest('dist/img'));
+})
 
 gulp.task('styles', function() {
 	gulp.src('sass/**/*.scss')
@@ -21,3 +55,17 @@ gulp.task('styles', function() {
 		.pipe(gulp.dest('dist/css'))
 		.pipe(browserSync.stream());
 });
+
+gulp.task('lint', function () {
+	return gulp.src(['js/**/*.js'])
+		// eslint() attaches the lint output to the eslint property
+		// of the file object so it can be used by other modules.
+		.pipe(eslint())
+		// eslint.format() outputs the lint results to the console.
+		// Alternatively use eslint.formatEach() (see Docs).
+		.pipe(eslint.format())
+		// To have the process exit with an error code (1) on
+		// lint error, return the stream and pipe to failOnError last.
+		.pipe(eslint.failOnError());
+});
+
