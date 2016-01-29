26c6ad4840267785c7247e2d2ebfa639ff0f5622
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2016-01-29 09:31:12 +0900

    adding dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
 
 gulp.task('default', ['copy-html', 'copy-images', 'styles', 'lint'], function() {
 	//default tasks go here
 	gulp.watch('sass/**/*.scss', ['styles']);
 
+	browserSync.init({
+		server: './dist'
+	});
+});
 
+gulp.task('styles', function() {
+	gulp.src('sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(gulp.dest('dist/css'))
+		.pipe(browserSync.stream());
 });
