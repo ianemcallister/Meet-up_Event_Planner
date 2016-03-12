5bbd9c52f79b809db7098c759529ce788967c116
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2016-03-13 06:28:46 +0700

    new user validations working

--- dist/js/controllers/LandingPage.controller.js
+++ dist/js/controllers/LandingPage.controller.js
@@ -2,13 +2,17 @@ angular
     .module('meetUpEventApp')
     .controller('LandingPageController', LandingPageController);
 
-LandingPageController.$inject = ['$log'];
+LandingPageController.$inject = ['$log', '$location'];
 
-function LandingPageController($log) {
+function LandingPageController($log, $location) {
 	var vm = this;
 
 	//define local variables
-	vm.showReuirnments = false;
+	vm.showRequirnments = false;
+	vm.validNewName = {'valid':false, 'style':{color:''}};
+	vm.validNewEmail = {'valid':false, 'style':{color:''}};
+	vm.securePassword = {'secure':false, 'style':{color:''}};
+	vm.unlockCreateUserBtn = {'usable':false, 'class':'btn btn-warning'};
 	vm.passwordRequirnments = {
 		0:{'constraint':'Is at least 16 characters long', 'style':{color:'red'}, 'met':false},
 		1:{'constraint':'Is no longer than 100 characters', 'style':{color:'red'}, 'met':false},
@@ -22,36 +26,133 @@ function LandingPageController($log) {
 	//define controller methods
 	vm.displayPassReqs = function() {
 		if(vm.newName && vm.newEmail) {
-			vm.showReuirnments = true;
+			vm.showRequirnments = true;
 		} else {
-			vm.showReuirnments = false;
+			vm.showRequirnments = false;
 		}
 	}
 
-	vm.checkNewPasswords = function(newUserPassword) {
+	vm.checkNewName = function() {
+		if(vm.newName.length > 0) {
+			vm.validNewName.valid = true;
+			vm.validNewName.style = {color:'green'};
+		} else {
+			vm.validNewName.valid = false;
+			vm.validNewName.style = {color:'red'};
+		}
+
+		vm.openCreateUserButton();
+	}
+
+	vm.checkNewEmail = function () {
+		//check validity
+		var constraint = new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", "");
+
+		if(constraint.test(vm.newEmail)) {
+			vm.validNewEmail.valid = true;
+			vm.validNewEmail.style = {color:'green'};
+		} else {
+			vm.validNewEmail.valid = false;
+			vm.validNewEmail.style = {color:'red'};
+		}
+		
+		vm.openCreateUserButton();
+	}
+
+	vm.checkNewPasswords = function() {
 		//for each password define the pattern that the password must match
 		var constraints = {
-	 		0: ['[\!\@\#\$\%\^\&\*]', " no required symbols found"],
-	 		1: ['[0-9]', " no numbers found"],
-	 		2: ['[a-z]', " no lowercase letters found"],
-	 		3: ['[A-Z]', " no uppercase letters found"],
-	 		4: ['[^A-z0-9\!\@\#\$\%\^\&\*]', " illegal characters found"]
+	 		0: ['', ' your password needs to be 16 characters or longer'],
+	 		1: ['', ' your password needs to be less than 100 characters'],
+	 		2: ['[\!\@\#\$\%\^\&\*]', " no required symbols found"],
+	 		3: ['[0-9]', " no numbers found"],
+	 		4: ['[a-z]', " no lowercase letters found"],
+	 		5: ['[A-Z]', " no uppercase letters found"],
+	 		6: ['[^A-z0-9\!\@\#\$\%\^\&\*]', " illegal characters found"],
  		};
+ 		
  		//check for length
  		if(vm.newPassword) {
- 			if(vm.newPassword.length > 15) vm.passwordRequirnments[0].style = {color:'green'};
- 			if(vm.newPassword.length <= 100) vm.passwordRequirnments[1].style = {color:'green'};
+ 			if(vm.newPassword.length > 15) { 
+ 				vm.passwordRequirnments[0].style = {color:'green'}; 
+ 				vm.passwordRequirnments[0].met = true; 
+ 			} else {
+ 				vm.passwordRequirnments[0].style = {color:'red'}; 
+ 				vm.passwordRequirnments[0].met = false;
+ 			}
+ 			if(vm.newPassword.length <= 100) { 
+ 				vm.passwordRequirnments[1].style = {color:'green'}; 
+ 				vm.passwordRequirnments[1].met = true;
+ 			} else {
+ 				vm.passwordRequirnments[1].style = {color:'red'}; 
+ 				vm.passwordRequirnments[1].met = false;
+ 			}
 
+ 			//check constraints 2-5
  			for(i = 2; i < 6; i++) {
 	 			//check for required symbols, uppercase, losercase, and characters
 	 			var constraint = new RegExp(constraints[i][0], "");	
 
-	 			if(constraint.test(vm.newPassword.value)) vm.passwordRequirnments[i].style = {color:'green'};
+	 			//test each constratint
+	 			if(constraint.test(vm.newPassword)) {
+	 				vm.passwordRequirnments[i].style = {color:'green'};
+	 				vm.passwordRequirnments[i].met = true;
+	 			} else {
+	 				vm.passwordRequirnments[i].style = {color:'red'};
+	 				vm.passwordRequirnments[i].met = false;
 	 			}
+	 		}
 
+	 		//check illegal characters
+	 		var constraint = new RegExp(constraints[6][0], "");
+
+	 		//run the check
+	 		if(!constraint.test(vm.newPassword)) {
+	 			vm.passwordRequirnments[6].style = {color:'red'};
+	 			vm.passwordRequirnments[6].met = false;
+	 		} else {
+	 			vm.passwordRequirnments[6].style = {color:'green'};
+	 			vm.passwordRequirnments[6].met = true;
+	 		}
+
+	 		//if all constrains met, throw valid pass flag
+	 		if( vm.passwordRequirnments[0].met &&
+	 			vm.passwordRequirnments[1].met &&
+	 			vm.passwordRequirnments[2].met &&
+	 			vm.passwordRequirnments[3].met &&
+	 			vm.passwordRequirnments[4].met &&
+	 			vm.passwordRequirnments[5].met &&
+	 			vm.passwordRequirnments[6].met) {
+	 			vm.securePassword.secure = true
+	 		} else {
+	 			vm.securePassword.secure = false;
+	 		}
+
+	 		if(vm.securePassword.secure) {
+	 			vm.securePassword.style = {color:'green'};
+	 		} else {
+	 			vm.securePassword.style = {color:'red'};
+	 		}
  		}
  		
- 	
+ 		vm.openCreateUserButton();
+	}
+
+	vm.openCreateUserButton = function() {
+		if(vm.validNewName.valid && vm.validNewEmail.valid && vm.securePassword.secure) {
+			vm.unlockCreateUserBtn.class = 'btn btn-primary';
+			vm.unlockCreateUserBtn.usable = true;
+		} else {
+			vm.unlockCreateUserBtn.class = 'btn btn-warning';
+			vm.unlockCreateUserBtn.usable = false;	
+		}
+		
+	}
+
+	vm.createNewUser = function() {
+		if(vm.unlockCreateUserBtn.usable) {
+			$location.path('/newUser');
+		}
 	}
 
 }
