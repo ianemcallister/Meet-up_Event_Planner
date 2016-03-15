69c24ee6d9f6bb5042f2ddda7b7f75a82c875a02
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2016-03-15 13:41:47 +0700

    breaking event up into three sections

--- js/controllers/AnEvent.controller.js
+++ js/controllers/AnEvent.controller.js
@@ -7,9 +7,43 @@ AnEventController.$inject = ['$log', '$location', 'userData'];
 function AnEventController($log, $location, userData) {
 	var vm = this;
 
+	//declare and initialize local variables
+	vm.event = {
+		id: 0,
+		name: '',
+		type: '',
+		host: '',
+		startTime: 0,
+		endTime: 0,
+		message: '',
+		guestList: {},
+		address: {}
+	};
+
+	vm.currentSection = 1;
+	vm.manageSections = {
+		1: {active: true, complete: false, style:{color:'black', 'background-color':'yellow'}},
+		2: {active: false, complete: false, style:{color:'white', 'background-color':'gray'}},
+		3: {active: false, complete: false, style:{color:'white', 'background-color':'gray'}}
+	};
+
 	$log.info('into a single event controller');
 
 	vm.submit = function() {
 		$log.info('submitting the form now!');
+	}
+
+	vm.changeSection = function(targetSection) {
+		for(i = 1; i <=3; i++) {
+			if(i==targetSection) {
+				vm.manageSections[i].active = true;
+				vm.manageSections[i].style['background-color'] ='yellow';
+				vm.manageSections[i].style['color'] ='black';
+			} else {
+				vm.manageSections[i].active = false;
+				vm.manageSections[i].style['background-color'] ='gray';
+				vm.manageSections[i].style['color'] ='white';
+			}
+		}
 	}
 }
