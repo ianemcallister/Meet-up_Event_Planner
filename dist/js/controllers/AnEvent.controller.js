0e55ca1f2657d4cec1164832f1b5077d0c2322a1
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2016-03-15 08:17:47 +0700

    newEvent form implamented

--- dist/js/controllers/AnEvent.controller.js
+++ dist/js/controllers/AnEvent.controller.js
@@ -5,5 +5,11 @@ angular
 AnEventController.$inject = ['$log', '$location', 'userData'];
 
 function AnEventController($log, $location, userData) {
+	var vm = this;
+
 	$log.info('into a single event controller');
+
+	vm.submit = function() {
+		$log.info('submitting the form now!');
+	}
 }
