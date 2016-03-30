d58ecff98dabce1a8c45f6fb3a3ed36b18cff823
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2016-03-30 13:56:36 +0700

    fix: Toolbar Login Wired Correctly
    Got the toolbar login and login all wired up so they should work correctly now

--- js/directives/ToolBar.directive.js
+++ js/directives/ToolBar.directive.js
@@ -26,27 +26,106 @@ function toolBar() {
 	function linkFunc(scope, el, attr, ctrl) {
     }
 
-    ToolBarController.$inject = ['$log', 'backendServices'];
+    ToolBarController.$inject = ['$scope', '$log', 'backendServices', 'trafficValet'];
     /* @ngInject */
-    function ToolBarController($log, backendServices) {
+    function ToolBarController($scope, $log, backendServices, trafficValet) {
 	    var vm = this;
 
 	    //local controler variables
 	    var fbConnect = backendServices;
+	    var toolbarSherpa = trafficValet;
 
 	    //local view variables
-		fbConnect.checkLoginStatus()
-		.then(function(status) {
-			vm.loggedIn = status;
-		})
-		.catch()
+		function checkLoginStatus() {
 
-		vm.clicked = function() {
+			fbConnect.checkLoginStatus()
+			.then(function(status) {
+				$log.info('toolbar checking login: ' + status);
+				vm.loggedIn = status;
+			})
+			.catch()
+		}
+
+		//vm methods
+		vm.login = function() {
 			$log.info('button clicked');
+			$log.info(vm.email + ' ' + vm.password);
+
+		}
+
+		vm.logout = function() {
+			$log.info('logging out');
+			
+			//logout
+			fbConnect.logUserOut();
+			
+			//redirect
+			toolbarSherpa.redirectTo('/login');
+		}
+
+		vm.loginRegisteredUser = function() {
+			//declare local variables
+			var registeredUserSherpa = trafficValet;
+
+			//confirm all fields are valid
+			if(true) {
+				//declare local variables
+				var database = backendServices;
+				var registeredUserData = userData;
+
+				//verify users credentials
+				database.LoginRegisteredUser(vm.email, vm.password)
+				.then(function(userCredentials) {
+					//if no trouble logging in update error object
+					//vm.errors.passesAllTests = true;
+					$log.info(userCredentials);
+					//add primary information to local model
+					//registeredUserData.setPrimariesLocally(userCredentials.email, userCredentials.name, userCredentials.uid);
+					return userCredentials.uid
+				})
+				.then(function(uid) {
+					$log.info("uid is " + uid);
+
+					database.getUserBio(uid)
+					.then(function(userBio) {
+						//update userData model with userBio
+						registeredUserData.updateBioLocally(userBio);
+
+						//load user events
+						database.getUserEvents(uid)
+						.then(function(allEventsForThisUser) {
+							//update userData model with userEvents
+							registeredUserData.updateAllUserEventsLocally(allEventsForThisUser);
+						})
+						.catch(function(message) { $log.info(message); })
+
+					})
+					.catch(function(message) { $log.info(message); })
+
+					//redirect to the next page 
+					registeredUserSherpa.redirectTo('/userInformation', uid);
+
+				})
+				.catch(function(message) { 
+					//if there was an error logging in, let the user know
+					//vm.errors.message = message;
+					//vm.errors.passesAllTests = false;
+				})
+
+				//use uid to collect user bio
+				
+			}
+
 		}
 
 		//actions
-		
+		checkLoginStatus();
+
+		//watchers
+		$scope.$on('$routeChangeStart', function(next, current) {
+			$log.info('the route chagned (says the tool bar)');
+			checkLoginStatus();
+		})
 	}
 
 	return  directive;
